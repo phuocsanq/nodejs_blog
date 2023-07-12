2492ac107b6f4e0b522ae9109f31144d42cca893
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-07-12 15:31:07 +0700

    deleted count courses

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,11 +1,24 @@
 const Course = require('../models/Course');
 
 class MeController {
+
     storedCourses(req, res, next) {
-        Course.find({}).lean()
-            .then((courses) => res.render('me/storedcourses', { courses }))
-            .catch(next);
+        Promise.all([Course.find({}).lean(), Course.countDocumentsWithDeleted({deleted:true})])
+            .then(([courses, deletedCount]) => res.render('me/storedcourses', {courses, deletedCount}))
+            .catch(next)
     }
+
+    // storedCourses(req, res, next) {
+    //     Course.find({}).lean()
+    //         .then((courses) => res.render('me/storedcourses', { courses }))
+    //         .catch(next);
+    // }
+
+    // countCourseDeleted(req, res, next) {
+    //     Course.countDocumentsDeleted()
+    //         .then((countDelete) => res.render('me/storedcourses', { countDelete }))
+    //         .catch(next)
+    // }
 
     trashCourses(req, res, next) {
         Course.findWithDeleted({deleted:true}).lean()
